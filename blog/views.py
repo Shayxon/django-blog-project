17fca676807bc16c2fe989f63a1f2e13265da2da
Shayxon <shayxon1@gmail.com>
2024-01-26 01:32:11 +0500

    add form to send posts by email

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id = post_id, status = Post.Status.PUBLISHED)
+    sent = False
+    
+    if request.method == 'POST':
+
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+
+            cd = form.cleaned_data
+
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"Hi! my name is {cd['name']}. I recommend you to read {post.title}"
+            comments = f"read {post.title} at {post_url}\n\n my comment: {cd['comment']}"
+
+            send_mail(subject, comments, cd['email'], [cd['to']], fail_silently=False)
+
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
 def post_list(request):
     post_list = Post.published.all()
--- mysite/settings.py
+++ mysite/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# EMAIL TESTING
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
