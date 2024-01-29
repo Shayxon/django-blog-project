b09f25b9de2b1b460e3461dcd5420cb45fcd684c
Shayxon <shayxon1@gmail.com>
2024-01-29 19:30:02 +0500

    search with trigram similarity

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id = post_id, status = Post.Status.PUBLISHED)
@@ -111,12 +111,9 @@ def post_search(request):
         if form.is_valid():
 
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
-            search_query = SearchQuery(query)
 
             results = Post.published.annotate(
-                search = search_vector,
-                rank = SearchRank(search_vector, search_query)
-            ).filter(rank__gte = 0.3).order_by('-rank')
+                similarity = TrigramSimilarity('title', query)
+            ).filter(similarity__gt = 0.1).order_by('-similarity')
 
     return render(request, 'blog/post/search.html', {'results': results, 'query': query, 'form': form})
